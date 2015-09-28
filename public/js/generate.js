9b477d2555ba4c6a7bb7b0762fd7909070be9f2f
Greenstick <benacordier@gmail.com>
2015-09-28 11:02:29 -0700

    Version 0.30 - Update

--- public/js/generate.js
+++ public/js/generate.js
@@ -234,8 +234,8 @@ generateUserData - Output Schema
 				bloodPressure 	= {sbp: sbp, dbp: dbp},
 				heartRate 		= Math.floor(((0.4 + (Math.random() * 0.1) + (seed * 0.3)) * 128) + ((bmi / 42) * 0.2)),
 				sleepiness 		= Math.floor(((Math.random() * 0.3) + (((8 / cpapHrs) / 8) * 0.6) + (seed * 0.1)) * 10),
-				sleepQuality	= Math.floor(((Math.random() * 0.2) + ((cpapHrs / 8) * 0.7) + (seed * 0.1)) * 10),
-				arousalCount 	= Math.floor(((Math.random() * 0.2) + (((8 / cpapHrs) / 8) * 0.6) + (seed * 0.2)) * 180),
+				sleepQuality	= Math.floor(((Math.random() * 0.1) + ((cpapHrs / 8) * 0.8) + (seed * 0.1)) * 10),
+				arousalCount 	= Math.floor(((Math.random() * 0.2) + (((8 / cpapHrs) / 8) * 0.5) + (seed * 0.3)) * 80),
 				datetime 		= rawDate.toISOString().split('T')[0];
 				sleepiness 		= isFinite(sleepiness) 		? sleepiness 	: 10;
 				arousalCount 	= isFinite(arousalCount) 	? arousalCount 	: 12;
